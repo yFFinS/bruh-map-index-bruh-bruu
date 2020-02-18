03a4972cd353ab6afa7cc50f3100b9a511801f2b
yFFinS <43855058+yFFinS@users.noreply.github.com>
2020-02-18 15:51:47 +0500

    Added text field, buttons, everything you need

--- interface.py
+++ interface.py
@@ -24,6 +24,7 @@ class SimpleInterfaceObject(pg.sprite.Sprite):
             self.rect.topleft = pos
 
     def render_image(self):
+        self.image.fill(self.bg_color)
         size = self.rect.size
         coords = [(self.borders_width, self.borders_width),
                   (size[0] - self.borders_width, self.borders_width),
@@ -41,22 +42,26 @@ class DynamicInterfaceObject(SimpleInterfaceObject):
     def __init__(self, pos=(0, 0), size=(0, 0),
                  bg_color=(255, 255, 255), borders_color=(255, 255, 255),
                  borders_width=1, action=None, args=(), **kwargs):
-        super().__init__(pos, size, bg_color, borders_color, borders_width, **kwargs)
+        super().__init__(pos=pos, size=size,
+                         bg_color=bg_color, borders_color=borders_color, borders_width=borders_width, **kwargs)
         self.action = action
         self.args = args
 
         self.hovered = False
+        self.focused = False
         self.pressed = False
 
     def update(self, event):
         if event.type is MOUSEMOTION:
-            self.hovered = self.rect.collidepoint(event.pos)
+            self.hovered = bool(self.rect.collidepoint(event.pos))
         if event.type is MOUSEBUTTONDOWN:
             if event.button is BUTTON_LEFT:
                 self.pressed = self.hovered
+                self.focused = self.hovered
         if event.type is MOUSEBUTTONUP:
-            if self.pressed:
+            if self.rect.collidepoint(event.pos) and self.pressed:
                 self.on_click_action()
+            self.pressed = False
 
     def on_click_action(self):
         if self.action is not None:
@@ -68,31 +73,60 @@ class Label(SimpleInterfaceObject):
     def __init__(self, pos=(0, 0), size=(0, 0), text='', font=None, text_color=(0, 0, 0),
                  bg_color=(255, 255, 255), borders_color=(255, 255, 255),
                  borders_width=1, **kwargs):
-        super().__init__(pos, size, bg_color, borders_color, borders_width, **kwargs)
+        super().__init__(pos, size, bg_color, borders_color, borders_width)
         self.text = text
         self.text_color = text_color
         self.font = pg.font.Font(None, 20) if font is None else font
+        self.prevent_unfitted_text = True
         self.rendered_text = None
         self.render_text()
 
-    def render_text(self):
+    def render_text(self, center=False):
+        self.render_image()
         size = self.rect.size
         line = self.font.render(self.text, True, self.text_color)
         rect = line.get_rect(center=(size[0] // 2, size[1] // 2))
-        self.render_image()
+        if not center:
+            rect.x = 2 * self.borders_width + 1
         self.image.blit(line, rect)
 
-    def set_text(self, text):
+    def set_text(self, text, center=False):
+        if self.prevent_unfitted_text and self.font.size(text)[0] > self.rect.w - 2 * (2 * self.borders_width + 1):
+            return
         self.text = text
-        self.render_text()
+        self.render_text(center)
 
 
-class Button(Label, DynamicInterfaceObject):
+class Button(DynamicInterfaceObject, Label):
 
     def __init__(self, pos=(0, 0), size=(0, 0), text='', font=None, text_color=(0, 0, 0),
                  bg_color=(255, 255, 255), borders_color=(255, 255, 255),
                  borders_width=1, action=None, args=(), **kwargs):
-        super().__init__(pos, size, text=text, font=font, text_color=text_color,
+        super().__init__(pos, size,
                          bg_color=bg_color, borders_color=borders_color, borders_width=borders_width,
-                         action=action, args=args)
+                         action=action, args=args, text=text, font=font, text_color=text_color)
 
+
+class TextField(DynamicInterfaceObject, Label):
+
+    def __init__(self, pos=(0, 0), size=(0, 0), init_text='', font=None, text_color=(0, 0, 0),
+                 bg_color=(255, 255, 255), borders_color=(255, 255, 255),
+                 borders_width=1, **kwargs):
+        super().__init__(pos, size, text='', font=font, text_color=text_color,
+                         bg_color=bg_color, borders_color=borders_color, borders_width=borders_width,
+                         action=None, args=())
+
+        self.init_text = init_text
+
+    def update(self, event):
+        super().update(event)
+
+        if self.focused and event.type is KEYDOWN:
+            if event.key is K_BACKSPACE and self.text:
+                self.set_text(self.text[:-1])
+            elif event.key is K_RETURN:
+                self.focused = False
+            elif KMOD_SHIFT & pg.key.get_mods():
+                self.set_text(self.text + event.unicode.upper())
+            else:
+                self.set_text(self.text + event.unicode)
